e6c20ebfec8758c2dd250080e503c4aa9e3ed8d0
cctsba25231 <sba25231@student.cct.ie>
2026-07-06 12:46:34 +0100

    Create DIP_AI_NuriaCanestroPallas_sba25231_L7.py

---
+++ DIP_AI_NuriaCanestroPallas_sba25231_L7.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Freemont Bridge Bicycle Dashboard"
+    layout="wide"
+)
+
+st.title("Freemont Bridge Bicycle Dashboard")
+
+#Load Data
+df = pd.read_csv("Freemont_Bridge_Bycle_counter.csv")
+
+#Rename columns to make them easier to use
+df=df.rename(columns={
+    "Date": "date",
+     "Fremont Bridge Sidewalks, south of N 34th St Total": "total",
+    "Fremont Bridge Sidewalks, south of N 34th St Cyclist West Sidewalk": "west_sidewalk",
+    "Fremont Bridge Sidewalks, south of N 34th St Cyclist East Sidewalk": "east_sidewalk"
+})
+
+# Convert date column to datetime
+df["date"] = pd.to_datetime(df["date"], errors="coerce")
+
+# Remove rows with missing or invalid dates
+df = df.dropna(subset=["date"])
+
+# Replace missing bicycle counts with 0
+df["total"] = df["total"].fillna(0)
+df["west_sidewalk"] = df["west_sidewalk"].fillna(0)
+df["east_sidewalk"] = df["east_sidewalk"].fillna(0)
+
+# Make count columns whole numbers
+df["total"] = df["total"].astype(int)
+df["west_sidewalk"] = df["west_sidewalk"].astype(int)
+df["east_sidewalk"] = df["east_sidewalk"].astype(int)
+
+# Create useful date columns
+df["year"] = df["date"].dt.year
+df["month"] = df["date"].dt.month
+df["day"] = df["date"].dt.day
+df["hour"] = df["date"].dt.hour
+df["day_name"] = df["date"].dt.day_name()
+
+# Show cleaned data
+st.subheader("Cleaned Data Preview")
+st.dataframe(df.head())
+
+st.subheader("Dataset Information")
+st.write("Number of rows:", df.shape[0])
+st.write("Number of columns:", df.shape[1])
